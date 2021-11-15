48dcd1d5bab6283f6cd5c18103f524f9c0b4dc1c
meghanr93 <meghanr93@BHSCUR149024SPW.kprdsb.schools.local>
2021-11-15 12:17:15 -0500

    imports

--- src/main/java/ryan/battleship/FXMLController.java
+++ src/main/java/ryan/battleship/FXMLController.java
@@ -1,8 +1,8 @@
 package ryan.battleship;
 /*
-Put header here
-
-
+Meghan Ryan
+11/15/2021
+This program, you have to find randomly placed battleships within a limited number of turns.
  */
 
 import java.net.URL;
@@ -11,16 +11,97 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLController implements Initializable {
-    
+        
     @FXML
-    private Label lblOut;
-    
+    private ImageView imgGrid1;
     @FXML
-    private void btnClickAction(ActionEvent event) {
-        lblOut.setText("Hello World!");
+    private ImageView imgGrid2;
+    @FXML
+    private ImageView imgGrid3;
+    @FXML
+    private ImageView imgGrid4;
+    @FXML
+    private ImageView imgGrid5;
+    @FXML
+    private ImageView imgGrid6;
+    @FXML
+    private ImageView imgGrid7;
+    @FXML
+    private ImageView imgGrid8;
+    @FXML
+    private ImageView imgGrid9;
+    @FXML
+    private ImageView imgGrid10;
+    @FXML
+    private ImageView imgGrid11;
+    @FXML
+    private ImageView imgGrid12;
+    @FXML
+    private ImageView imgGrid13;
+    @FXML
+    private ImageView imgGrid14;
+    @FXML
+    private ImageView imgGrid15;
+    @FXML
+    private ImageView imgGrid16;
+    @FXML
+    private ImageView imgGrid17;
+    @FXML
+    private ImageView imgGrid18;
+    @FXML
+    private ImageView imgGrid19;
+    @FXML
+    private ImageView imgGrid20;
+    @FXML
+    private ImageView imgGrid21;
+    @FXML
+    private ImageView imgGrid22;
+    @FXML
+    private ImageView imgGrid23;
+    @FXML
+    private ImageView imgGrid24;
+    @FXML
+    private ImageView imgGrid25;
+    @FXML
+    private ImageView imgGrid26;
+    @FXML
+    private ImageView imgGrid27;
+    @FXML
+    private ImageView imgGrid28;
+    @FXML
+    private ImageView imgGrid29;
+    @FXML
+    private ImageView imgGrid30;
+    @FXML
+    private ImageView imgGrid31;
+    @FXML
+    private ImageView imgGrid32;
+    @FXML
+    private ImageView imgGrid33;
+    @FXML
+    private ImageView imgGrid34;
+    @FXML
+    private ImageView imgGrid35;
+    @FXML
+    private ImageView imgGrid36;
+
+    @FXML
+    private Label lblTurns;
+
+    @FXML
+    void btnExit(ActionEvent event) {
+
     }
+
+    @FXML
+    void imgClick(MouseEvent event) {
+
+    }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
